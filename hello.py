7e33ba12fc48b3d8398d66788ec0eafba6bb5b3f
Vijay Kumar <Vijjuk369@gmail.com>
2024-04-21 09:45:32 +0530

    hello file

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Hello!")
+
+My_Name = "Vijay"
+Your_Name = input("Hey stranger, May i konw your name. I requesting you to enter your name here : ")
+
+print(f"Its pleasure to know your name, my name is {My_Name.upper()} and nice to meet you {Your_Name.upper()}.")
+
+print("I appriciate your contribution, thanks")
