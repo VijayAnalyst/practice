3704764558a0e67d0750b6defe7804aa1d67975a
Vijay Kumar <Vijjuk369@gmail.com>
2024-04-24 23:39:58 +0530

    python.py

---
+++ Python.py
@@ -0,0 +1,53 @@
+#Hi! Welcome. If you are learning Python, then this article is for you. You will find a thorough description of Python syntax and lots of code examples to guide you during your coding journey.
+"""
+What we will cover:
+* Variable Definitions in Python
+* Hello, World! Program in Python
+* Data Types and Built-in Data Structures in Python
+* Python Operators
+* Conditionals in Python
+* For Loops in Python
+* While Loops in Python
+* Nested Loops in Python
+* Functions in Python
+* Recursion in Python
+* Exception Handling in Python
+* Object-Oriented Programming in Python
+* How to Work with Files in Python
+* Import Statements in Python
+* List and Dictionary Comprehension in Python
+* and more...
+"""
+#this syntax to create a variable and assign a value to this variable:
+# syntax : <var_name> = <value>
+#For example:
+
+age = 56
+name = "Nora"
+color = "Blue"
+grades = [67, 100, 87, 56]
+
+#Hello, World! Program in Python
+#Before we start diving into the data types and data structures that you can use in Python, let's see how you can write your first Python program.
+#You just need to call the print() function and write "Hello, World!" within parentheses:
+
+print("Hello, World!")
+print (name)
+
+#You will see this message after running the program:
+#"Hello, World!"
+
+#Data Types and Built-in Data Structures in Python
+#We have several basic data types and built-in data structures that we can work with in our programs. Each one has its own particular applications. Let's see them in detail.
+#Numeric Data Types in Python: Integers, Floats, and Complex
+
+#Integers
+#Integers are numbers without decimals. You can check if a number is an integer with the type() function. If the output is <class 'int'>, then the number is an integer.
+#For example:
+number = 1
+print(type(number))
+#<class 'int'>
+print(type(15))
+#<class 'int'>
+
+
